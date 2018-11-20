3b9473f297d02c7287d2373aca833f6b0e8c1719
Melmuh <38730780+Melmuh@users.noreply.github.com>
2018-11-20 12:54:36 +0100

    Nothing

---
+++ db.php
@@ -0,0 +1,14 @@
+<?php
+
+echo "# locksmith" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/Melmuh/locksmith.git
+git push -u origin master
+
+echo "Moin";
+
+?>
+
+
