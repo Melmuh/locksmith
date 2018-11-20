8a4d710af2558a2de95f93c8632d3633fd94d4eb
Melmuh <38730780+Melmuh@users.noreply.github.com>
2018-11-20 14:54:27 +0100

    Moin weg

--- db.php
+++ db.php
@@ -1,13 +1,6 @@
 <?php
 
-echo "# locksmith" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git remote add origin https://github.com/Melmuh/locksmith.git
-git push -u origin master
-
-echo "Moin";
+echo "Hallo";
 
 ?>
 
