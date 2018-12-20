4e6ca7d5b418e2d5512c121bc2175c2e54c1e165
Melmuh <38730780+Melmuh@users.noreply.github.com>
2018-12-20 17:58:54 +0100

    header-bild // htaccess

--- Webseite/Aussehen/Vorlagen/html/header.php
+++ Webseite/Aussehen/Vorlagen/html/header.php
@@ -1,27 +1,29 @@
 <?php ?>
 <header class="row">
     <ul class="nav justify-content-end bg-dark text-white" style="width:100%">
-    <li class="nav-item">
-        <a class="nav-link active" href="http://localhost/Onlineshop2/locksmith/index.php\">Home</a>
-    </li>
-    <li class="nav-item">
-        <a class="nav-link" href="http://localhost/Onlineshop2/locksmith/spiele.php\">Spiele</a>
-    </li>
-    <li class="nav-item">
-        <a class="nav-link" href="http://localhost/Onlineshop2/locksmith/kontakt.php\">Kontakt</a>
-    </li>
-    <!-- <li class="nav-item">
-        <a class="nav-link" href="#">Warenkorb</a>
-    </li> -->
-    <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="" role="button" aria-haspopup="true" aria-expanded="false">Mein Konto</a>
-            <div class="dropdown-menu">
-                <a class="dropdown-item" href="http://localhost/Onlineshop2/locksmith/kontodetails.php\">Kontodetails</a>
-                <a class="dropdown-item" href="http://localhost/Onlineshop2/locksmith/warenkorb.php\">Warenkorb</a>
-                <div class="dropdown-divider"></div>
-                <a class="dropdown-item" href="http://localhost/Onlineshop2/locksmith/abmelden.php\">Abmelden</a>
-            </div>
-    </li>
+        <li class="nav-item">
+            <a class="nav-link active" href="http://localhost/Onlineshop2/locksmith/index.php\">Home</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="http://localhost/Onlineshop2/locksmith/spiele.php\">Spiele</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="http://localhost/Onlineshop2/locksmith/kontakt.php\">Kontakt</a>
+        </li>
+        <!-- <li class="nav-item">
+            <a class="nav-link" href="#">Warenkorb</a>
+        </li> -->
+        <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="" role="button" aria-haspopup="true" aria-expanded="false">Mein Konto</a>
+                <div class="dropdown-menu">
+                    <a class="dropdown-item" href="http://localhost/Onlineshop2/locksmith/kontodetails.php\">Kontodetails</a>
+                    <a class="dropdown-item" href="http://localhost/Onlineshop2/locksmith/warenkorb.php\">Warenkorb</a>
+                    <div class="dropdown-divider"></div>
+                    <a class="dropdown-item" href="http://localhost/Onlineshop2/locksmith/abmelden.php\">Abmelden</a>
+                </div>
+        </li>
     </ul>
-    <img src="/Onlineshop2/locksmith/webseite/aussehen/vorlagen/Images/header-min.jpg" alt="Responsive image" class="img-fluid">
+    <div>
+        <img src="/Onlineshop2/locksmith/webseite/aussehen/vorlagen/Images/header-min.jpg" alt="Responsive image" class="img-fluid">
+    </div>
 </header>
